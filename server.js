c0d0fd1fad0938f0219e7a8b2577a8beb96ff885
dhararaghu777 <dhararaghu777@gmail.com>
2022-06-04 23:18:54 +0530

    final commit 04-06-22

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const connectDB = require('../config/db')
+const connectDB = require('./config/db')
 const cors = require('cors')
 const path= require('path')
 
@@ -20,20 +20,20 @@ connectDB((err, res) => {
 
 
 // API routes
-app.use('/api/signin', require('../routes/api/signIn'))
-app.use('/api/login', require('../routes/api/login'))
-app.use('/api/users', require('../routes/api/users'))
-app.use('/api/profilePic', require('../routes/api/profilePic'))
-app.use('/api/profile', require('../routes/api/profile'))
-app.use('/api/myprofile', require('../routes/api/myprofile'))
-app.use('/api/admin', require('../routes/api/adminDetails'))
+app.use('/api/signin', require('./routes/api/signIn'))
+app.use('/api/login', require('./routes/api/login'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profilePic', require('./routes/api/profilePic'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/myprofile', require('./routes/api/myprofile'))
+app.use('/api/admin', require('./routes/api/adminDetails'))
 
 //serve static assets in production
 
 if(process.env.NODE_ENV === 'production'){
 
   //set static folder
-  app.use(express.static('/client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
