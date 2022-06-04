b3900bc5a513bbb4dbb5a796fdfed2ab7b2c99a4
dhararaghu777 <dhararaghu777@gmail.com>
2022-06-04 21:51:39 +0530

    post deployment 6

--- server.js
+++ server.js
@@ -30,22 +30,17 @@ app.use('/api/admin', require('./routes/api/adminDetails'))
 
 //serve static assets in production
 
-__dirname= path.resolve()
 if(process.env.NODE_ENV === 'production'){
 
   //set static folder
-  app.use(express.static(path.join(__dirname,'client/build')))
+  app.use(express.static('client/build'));
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve('client', 'build', 'index.html'))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
   
 }
-else{
-  app.get('/', (req, res)=>{
-    res.send("Backend running")
-  })
-}
+
 
 // launching server
 app.listen(PORT, ()=>{
